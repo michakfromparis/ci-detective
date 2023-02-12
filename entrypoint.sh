b3daed7856173607b2e19d1764663b7954d6e1fd
Michel Courtine <michak@michak.net>
2023-02-12 16:47:58 +0100

    :sparkles: feat(env): added support for environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -l
 
-# INPUT_PACKAGE_JSON=true
+# INPUT_INSPECT_SYSTEM=false
+# INPUT_INSPECT_GIT=false
+# INPUT_INSPECT_PACKAGE_JSON=true
 # INPUT_PACKAGE_JSON_PATH='./package.json'
-# INPUT_SYSTEM_INFO=false
-# INPUT_GIT_INFO=false
+# INPUT_INSPECT_ENVIRONMENT='APP_ENV,SHELL, USER, SOMETHING,       SOMETHING_ELSE'
 
 function draw_frame() {
   local msg="$1"
@@ -17,7 +18,7 @@ function draw_frame() {
 
 get_env() { [ -z "$(eval echo "\$$1")" ] && echo "$1 is undefined" || eval echo "\$$1"; }
 
-function system_info() {
+function inspect_system() {
   echo "🖥️  Runner hostname : $(uname -n)"
   echo "💻  Runner name     : $(get_env RUNNER_NAME)"
   echo "🌐  Runner OS       : $(get_env RUNNER_OS)"
@@ -26,7 +27,7 @@ function system_info() {
   echo "🚧  uname -a        : $(uname -a)"
 }
 
-function git_info() {
+function inspect_git() {
   echo "🌎  Detected environment: $(get_env APP_ENV)"
   version_tag=$(git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null) || version_tag='No version tag detected'
   echo "📁  Git branch name  : $(git rev-parse --abbrev-ref HEAD)"
@@ -37,7 +38,19 @@ function git_info() {
   echo "🔄  Git commit range : $(get_env GITHUB_SHA)...$(get_env GITHUB_EVENT_BEFORE)"
 }
 
-function package_info() {
+function inspect_environment() {
+  inspect_environment=$(echo "$INPUT_INSPECT_ENVIRONMENT" | tr ',' '\n')
+  for variable in $inspect_environment; do
+    value=$(printenv "$variable")
+    if [ -z "$value" ]; then
+      echo "❌ $variable is not set"
+    else
+      echo "✅ $variable: $value"
+    fi
+  done
+}
+
+function inspect_package() {
   local package_json_path=${INPUT_PACKAGE_JSON_PATH:-package.json}
   if [ ! -f "$package_json_path" ]; then
     echo "❌  Error: package.json not found at $package_json_path"
@@ -48,8 +61,8 @@ function package_info() {
   echo "📜  name               : ${name}"
   echo "📦  version            : ${version}"
   # Compare package.json version with tag version
-  echo "🔍  Comparing package.json version with tag version:"
   if [ "${version_tag}" != 'No version tag detected' ] && [ "${version_tag:0:1}" == 'v' ]; then
+    echo "🔍  Comparing package.json version with tag version:"
     if [ "${version_tag}" != "v${version}" ]; then
       echo "  ⚠️  Warning: package.json version (${version}) does not match tag version (${version_tag})"
     else
@@ -60,19 +73,24 @@ function package_info() {
 
 draw_frame "🕵️ Build information detective results"
 
-if [ "${INPUT_SYSTEM_INFO:-true}" != "false" ]; then
-  draw_frame "📈 System Information"
-  system_info
+if [ "${INPUT_INSPECT_SYSTEM:-true}" != "false" ]; then
+  draw_frame "📈 System information"
+  inspect_system
 fi
 
-if [ "${INPUT_GIT_INFO:-true}" != "false" ]; then
+if [ "${INPUT_INSPECT_GIT:-true}" != "false" ]; then
   draw_frame "🧪 Git information:"
-  git_info
+  inspect_git
 fi
 
-if [ "${INPUT_PACKAGE_JSON:-false}" == "true" ] || [ -f "package.json" ]; then
-  draw_frame "📦  Package Information"
-  package_info
+if [ -n "${INPUT_INSPECT_ENVIRONMENT}" ]; then
+  draw_frame "🌎 Environment information:"
+  inspect_environment
+fi
+
+if [ "${INPUT_INSPECT_PACKAGE_JSON:-false}" == "true" ] || [ -f "package.json" ]; then
+  draw_frame "📦  Package information"
+  inspect_package
 else
   : # echo "ℹ️ Package information is not requested."
 fi
